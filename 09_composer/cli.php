04550df17beb0f870bdf9d19ec8bf20fccd8bc56
Shinpachi1009 <jamaricjes@gmail.com>
2025-09-22 09:58:10 +0800

    added cli

---
+++ 09_composer/cli.php
@@ -0,0 +1,15 @@
+<?php
+
+    require 'vendor/autoload.php'; // file that registers all the thirdparty packages
+
+    use GuzzleHttp\Client;
+
+    $client =  new Client();
+    $response = $client->request('GET' , 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+    $body= $response->getBody();
+    $data = json_decode($body);
+
+    echo 'Bitcoin price in USD: ' . $data->bitcoin->usd;
+
+
+?>
